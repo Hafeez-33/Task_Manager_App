711dd88dbad07f9bd2a9fa5b0086c2c2538545fa
M Abdul Hafeez <4hafeez142@gmail.com>
2026-02-03 16:24:50 +0530

    fixes the error of Cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const path = require("path");
@@ -15,12 +15,22 @@ const reportRoutes = require("./routes/reportRoutes");
 const app = express();
 
 //Middleware to handle cors
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL || "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: ["Content-Type", "Authorization"],
+//     })
+// );
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-manager-app-k7hc.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
 );
 
 // CONNECT DATABASE
@@ -36,9 +46,7 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 
 //server uploads folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
